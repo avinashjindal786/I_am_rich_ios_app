0cac4aa4d9fc909798e71e4c66c3db3e0fbace17
avinashjindal786 <avinashjindal786@gmail.com>
2022-10-03 18:08:05 +0530

    Initial Commit

---
+++ i_am_rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_rich
+//
+//  Created by Avinash jindal on 03/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
